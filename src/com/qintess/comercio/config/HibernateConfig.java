935a022a8f479aad523eee487b05a8f29bdce69b
BruneiMS <bruneisiqueira@gmail.com>
2021-06-11 17:45:59 -0300

    Projeto criado

---
+++ src/com/qintess/comercio/config/HibernateConfig.java
@@ -0,0 +1,59 @@
+package com.qintess.comercio.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan("com.qintess.comercio.modelo");
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		
+		return sessionFactory;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/dbcomercio?useTimezone=trueuseUnicode=true&useLegacyDatetimeCode=false&serverTimezone=America/Sao_Paulo&characterEncoding=utf-8&useUnicode=true");
+		// os dados de user e pass precisam ser o de vocês!!!
+		dataSource.setUsername("postgres"); 
+		dataSource.setPassword("123");
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public HibernateTransactionManager getTransactionManager() {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory().getObject());
+		
+		return transactionManager;
+	}
+	
+	private final Properties hibernateProperties() {
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+		hibernateProperties.setProperty("hibernate.show_sql", "true");
+		hibernateProperties.setProperty("hibernate.connection.CharSet", "utf8");
+		hibernateProperties.setProperty("hibernate.connection.characterEncoding", "utf8");
+		hibernateProperties.setProperty("hibernate.connection.useUnicode", "true");
+		
+		return hibernateProperties;
+	}
+}
---
+++ src/com/qintess/comercio/modelo/Cliente.java
@@ -0,0 +1,88 @@
+package com.qintess.comercio.modelo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Cliente {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false, length = 15)
+	private String senha;
+	
+	@Column(nullable = false)
+	private LocalDate dtNascimento;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "cliente", fetch = FetchType.EAGER)
+	private List<Compra> compras;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public LocalDate getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(LocalDate dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+
+	public List<Compra> getCompras() {
+		return compras;
+	}
+
+	public void setCompras(List<Compra> compras) {
+		this.compras = compras;
+	}
+		
+}
